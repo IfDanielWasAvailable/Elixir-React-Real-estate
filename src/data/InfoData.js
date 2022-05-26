9a12c432b4826a3708026bb0fb0f720a2a3e4287
IfDanielWasAvailable <106308876+IfDanielWasAvailable@users.noreply.github.com>
2022-05-26 10:40:47 +0100

    Initial commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import ImageOne1 from'../images/ImageOne1.jpg';
+import ImageTwo2 from'../images/ImageTwo2.jpg';
+
+export const InfoData={
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis libero imperdiet, consectetur odio nec, lacinia purus. Sed mattis in.' ,
+    paragraphTwo:'Nam quis libero imperdiet, consectetur odio nec, lacinia purus. Sed mattis in.',
+    buttonLabel:'View Homes',
+    image:ImageOne1,
+    reverse:false,
+    delay:100
+};
+export const InfoDataTwo={
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor sit amet. Nam quis libero imperdiet, consectetur odio nec, lacinia purus. Sed mattis in.' ,
+    paragraphTwo:'Nam quis libero imperdiet, , lacinia purus. Sed mattis in.',
+    buttonLabel:'View Homes',
+    image:ImageTwo2,
+    reverse:true,
+    delay:300
+}
---
+++ src/data/SliderData.js
@@ -0,0 +1,45 @@
+import ImageOne from '../images/imageone.jpg';
+import ImageTwo from '../images/imagetwo.jpg';
+import ImageThree from '../images/imagethree.jpg';
+import ImageFour from '../images/imagefour.jpg';
+import ImageFive from '../images/imagefive.jpg';
+
+export const SliderData =[
+    {
+        title:'Luxury Vila in Bali, Indonesia',
+        price:'$4,270,000',
+        path:'/homes',
+        label:'View Home',
+        image: ImageOne,
+        alt:'House',
+        position:1
+        
+    },
+    {
+        title:'House in San Jose, Costa Rica',
+        price:'$2,740,000',
+        path:'/homes',
+        label:'View Home',
+        image: ImageTwo,
+        alt:'House',
+        position:2
+    },
+    {
+        title:'House in Calabases, California',
+        price:'$8,389,000',
+        path:'/homes',
+        label:'View Home',
+        image: ImageThree,
+        alt:'House',
+        position:3
+    },
+    {
+        title:'House in Houston, Texas',
+        price:'$4,620,000',
+        path:'/homes',
+        label:'View Home',
+        image: ImageFour,
+        alt:'House',
+        position:4
+    },
+]
